541cef10c247fab99e9d5e05fbd456e57f3761c8
thitinan-2008-webapp-labs <thitinan.ph@kkumail.com>
2023-07-17 22:42:31 +0700

    add lab5p2

---
+++ lab5p2/script/script.js
@@ -0,0 +1,38 @@
+let input;
+let inputInt;
+let list = [];
+average = 0;  
+min = 0;
+max = 0;
+
+function readInput() {
+  while (true) {
+    input = prompt("Enter an integer (a negative integer to quit):");
+    inputInt = parseInt(input);
+    if (inputInt < 0) {
+      break;
+    }
+
+    if (!isNaN(inputInt) && inputInt > 0) {
+      list.push(inputInt);
+    }
+  }
+  return list;
+}
+
+function displayStats(list) {
+  let totalNum = list.length > 0 ? list.length : "empty";
+
+  let sumNum = list.reduce((a, b) => a + b, 0);
+
+  average = totalNum > 0 ? (sumNum / totalNum) : 0;
+  min = totalNum > 0 ? Math.min(...list) : 0;
+  max = totalNum > 0 ? Math.max(...list) : 0;
+
+  alert(
+    `For the list that is ${totalNum} \nthe average is ${average.toFixed(2)} \nthe minimum is ${min} \nand the maximum is ${max}`
+  );
+}
+
+list = readInput();
+displayStats(list);
